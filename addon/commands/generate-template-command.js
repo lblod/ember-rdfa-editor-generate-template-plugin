8149ab8cd3e8c413a5d27ded8de1e85ab84a3d61
Oscar <lagartoverde97@gmail.com>
2022-09-20 12:10:37 +0200

    Add eslint

--- addon/commands/generate-template-command.js
+++ addon/commands/generate-template-command.js
@@ -1,4 +1,4 @@
-import defaultResources from "../utils/defaultResources";
+import defaultResources from '../utils/defaultResources';
 export default class GenerateTemplateCommand {
   name = 'generateTemplate';
 
@@ -12,19 +12,22 @@ export default class GenerateTemplateCommand {
 
   execute(controller) {
     let uris = [];
-    for(let resource of defaultResources) {
-      const quads = [...controller.datastore
-      .match(null, 'a', `>${resource}`)
-      .asQuads()];
-      const resourceUris = quads.map((quad) => quad.subject.value)
+    for (let resource of defaultResources) {
+      const quads = [
+        ...controller.datastore.match(null, 'a', `>${resource}`).asQuads(),
+      ];
+      const resourceUris = quads.map((quad) => quad.subject.value);
       uris = [...uris, ...resourceUris];
     }
     let documentHTML = controller._rawEditor.rootNode.outerHTML;
-    for(let uri of uris) {
+    for (let uri of uris) {
       const uriParts = uri.split('/');
       uriParts.pop();
       const uriWithoutUuid = uriParts.join('/');
-      documentHTML = documentHTML.replace(uri, `${uriWithoutUuid}/generateUuid()`)
+      documentHTML = documentHTML.replace(
+        uri,
+        `${uriWithoutUuid}/generateUuid()`
+      );
     }
     return documentHTML;
   }
